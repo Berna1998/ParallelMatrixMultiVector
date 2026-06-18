ab1a12e9f128ed1efbf5ab84c55ffce53d227fd8
Ardow22 <73550813+Ardow22@users.noreply.github.com>
2026-06-19 00:49:04 +0200

    Aggiornamento operations.c

--- utils/operations.c
+++ utils/operations.c
@@ -7,7 +7,6 @@ void genera_matrice(float* M, int x, int y){
   int i,j;
   for(i=0;i<x;i++){
     for(j=0;j<y;j++){
-       //M[i][j] = rand() % 100;
        M[i*y + j] = (float)rand() / (float)RAND_MAX;
     }
   }
@@ -17,14 +16,6 @@ void genera_matrice(float* M, int x, int y){
 
 void prodotto_seriale(float* A, float* X, int m, int n, int k, float* Y){
     int i,j,p;
-    
-    /*
-    int** Y = malloc(sizeof(int*)*n);
-    for(i=0;i<n;i++){
-      Y[i] = malloc(sizeof(int*)*k[z]);
-    }*/
-    //messa la calloc così che Y sia inizializzato con tutti valori pari a 0
-
     for(i=0;i<m;i++){
       for(p=0;p<n;p++){
         float a = A[i*n + p];
@@ -36,7 +27,7 @@ void prodotto_seriale(float* A, float* X, int m, int n, int k, float* Y){
 
 }
 
-// Funzione matmul: prodotto matrice-vettore/matrice
+//Funzione matmul: prodotto matrice-vettore/matrice
 void matmul(float* A, float* X, float* Y, int M, int N, int K){
     for(int i=0; i<M; i++){
         for(int j=0; j<K; j++){
@@ -54,7 +45,6 @@ double frobenius_error(float* Y, float* Y_serial, int m, int k) {
 
     for (int i = 0; i < m * k; i++) {
         double diff = (double)Y[i] - (double)Y_serial[i];
-       // printf("SERIALE: %e,  PARALLELO: %e\n", Y_serial[i], Y[i]);
 
         num += diff * diff;
         den += (double)Y_serial[i] * (double)Y_serial[i];
